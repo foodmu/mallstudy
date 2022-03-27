9870dd12d6caced40543139926b6d752db9cde01
cmq <3177783007@qq.com>
2022-03-27 19:35:50 +0800

    项目搭建完成

--- mall-order/src/test/java/com/jiuheng/mall/order/MallOrderApplicationTests.java
+++ mall-order/src/test/java/com/jiuheng/mall/order/MallOrderApplicationTests.java
@@ -1,13 +1,29 @@
 package com.jiuheng.mall.order;
 
+import com.jiuheng.mall.order.entity.OrderEntity;
+import com.jiuheng.mall.order.service.OrderService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @SpringBootTest
 class MallOrderApplicationTests {
 
+    @Resource
+    private OrderService orderService;
+
     @Test
     void contextLoads() {
+
+        OrderEntity orderEntity = new OrderEntity();
+        orderEntity.setBillReceiverEmail("317778007@qq.com");
+        boolean isSave = orderService.save(orderEntity);
+        if(isSave){
+            List<OrderEntity> orderLists = orderService.list();
+            orderLists.forEach(System.out::println);
+        }
     }
 
 }
--- mall-product/src/test/java/com/jiuheng/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/jiuheng/mall/product/MallProductApplicationTests.java
@@ -1,13 +1,36 @@
 package com.jiuheng.mall.product;
 
+import com.baomidou.mybatisplus.extension.conditions.query.QueryChainWrapper;
+import com.jiuheng.mall.product.entity.BrandEntity;
+import com.jiuheng.mall.product.service.BrandService;
+import com.jiuheng.mall.product.service.CategoryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @SpringBootTest
 class MallProductApplicationTests {
 
+    @Resource
+    private BrandService brandService;
+
     @Test
     void contextLoads() {
+
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("华为");
+        brandEntity.setDescript("nova7 128+8G");
+        // boolean save = brandService.save(brandEntity);
+//        if (save){
+//            System.out.println("保存成功......");
+//        }
+        List<BrandEntity> list1 = brandService.list();
+        list1.forEach(System.out::println);
+        brandService.removeById(1);
+        List<BrandEntity> list = brandService.list();
+        list.forEach(System.out::println);
     }
 
 }
